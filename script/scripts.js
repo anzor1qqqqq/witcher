9950708d4eb7f752ee044a54f0c5d5f233f68f90
Anzor <anzor.shadzhe@gmail.com>
2023-01-09 17:44:18 +0300

    deleted:    img/card-image1.svg 	deleted:    img/card-image2.svg 	modified:   index.html 	modified:   script/scripts.js 	modified:   style/style.css

--- script/scripts.js
+++ script/scripts.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let burgerMenu = () => {
+/*let burgerMenu = () => {
   const buttonBurger = document.querySelector('.button_burger');
   const headerContainer = document.querySelector('.header_container');
 
@@ -25,5 +25,106 @@ new Swiper('.swiper', {
       570: {
         slidesPerView: 2,
       }
+    }
+  });*/
+
+  const getElement = (tagName, classNames, atributes) => {
+    const element = document.createElement(tagName);
+
+    if (classNames) {
+      element.classList.add(...classNames)
+    };
+
+    if (atributes) {
+      for (const atribute in atributes) {
+        element[atribute] = atributes[atribute];
+      };
+    };
+
+    return element;
+  };
+
+  const createHeader = (param) => {
+    const header = getElement('header', ['header']);
+    const containerHeader = getElement('div', ['header_container']);
+    const navHeader = getElement('nav', ['header_nav']);
+    const navUl = getElement('ul', ['list_button']);
+    const contentSocial = getElement('div', ['content_social']);
+
+    if (param.header.logo) {
+      const logo = getElement('img', ['logo_img'], {
+        src: param.header.logo
+      });
+      
+      containerHeader.append(logo);
+    };
+
+    if (param.header.navButton) {
+      const arrNavButton = param.header.navButton;
+      arrNavButton.forEach((elem) => {
+        let ul = `
+            <li class="header_nav_button"><a href="#" class="header_nav_button_ur">${elem}</a></li>
+        `
+
+        navUl.insertAdjacentHTML('beforeend', ul);
+      });
+    };
+
+    if (param.header.socialLink) {
+        param.header.socialLink.map(item => {
+        const social = getElement('a', ['header_social']);
+        contentSocial.append(social);
+
+        social.append(getElement('img', [], {
+          src: item.img,
+        }));
+        
+        social.href = item.link;
+      })
+    };
+
+    header.append(containerHeader); 
+    containerHeader.append(navHeader);
+    navHeader.append(navUl);
+    containerHeader.append(contentSocial);
+
+    return header;
+  };
+
+  const movieConstructor = (selector, option) => {
+    const app = document.querySelector(selector);
+
+    if (option.header) {
+      app.append(createHeader(option));
+    };
+
+    if (option.backg) {
+      document.body.style.background = option.backg;
+    };
+  };
+
+  movieConstructor('.app', {
+    backg: 'url(/img/background.jpg) no-repeat top right 20%, #141218',
+    title: 'Ведьмак',
+    header: {
+      logo: '/img/logo1.svg',
+      navButton: ['Описание', 'Трейлер', 'Отзывы'],
+      socialLink: [
+        {
+          title: 'twitter',
+          link: '#',
+          img: '/img/twitter.svg'
+        },
+        {
+          title: 'instagram',
+          link: '#',
+          img: '/img/instagram.svg'
+        },
+        {
+          title: 'facebook',
+          link: '#',
+          img: '/img/facebook.svg'
+        },
+      ]
     }
   });
