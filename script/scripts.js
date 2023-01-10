278959e6bab7b79cedd0b3bd2ec27aa773c04115
Anzor <anzor.shadzhe@gmail.com>
2023-01-10 20:58:31 +0300

    modified:   script/scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -55,19 +55,20 @@ new Swiper('.swiper', {
       const logo = getElement('img', ['logo_img'], {
         src: param.header.logo
       });
-      
+
       containerHeader.append(logo);
     };
 
     if (param.header.navButton) {
       const arrNavButton = param.header.navButton;
+
       arrNavButton.forEach((elem) => {
         let ul = `
             <li class="header_nav_button"><a href="#" class="header_nav_button_ur">${elem}</a></li>
         `
 
         navUl.insertAdjacentHTML('beforeend', ul);
-      });
+      }); 
     };
 
     if (param.header.socialLink) {
@@ -91,8 +92,61 @@ new Swiper('.swiper', {
     return header;
   };
 
+  const mainContent = (param) => {
+    const main = getElement('main', []);
+    const mainContainer = getElement('div', ['container']);
+    const containerAbout = getElement('div', ['container_about']);
+    const containerStars = getElement('div', ['animate__animated',  'animate__fadeIn', 'container_ratting']);
+    const titleText = getElement('h1', ['animate__animated',  'animate__fadeIn', 'container_title'], {
+      textContent: param.title,
+    });
+    const pCnt = getElement('p', ['animate__animated',  'animate__fadeIn', 'container_text'], {
+          textContent: param.main.filmAbout,
+    });
+    const buttonMain = getElement('a', ['animate__animated',  'animate__fadeIn', 'container_button'], {
+      href: param.main.youTubeTrailer,
+      textContent: 'Смотреть трейлер',
+      target: '_blank',
+    });
+
+     if (param.main.ganre) {
+      param.main.ganre.map(item => {
+        const ganr = getElement('p', ['animate__animated',  'animate__fadeIn', 'genr_container'], {
+          textContent: item,
+      });
+
+        containerAbout.append(ganr);
+      });
+    }; 
+
+    if (param.main.ratting) {
+      param.main.ratting.map(item => {
+        const ratting = getElement('img', [], {
+          src: item.img,
+        });
+        containerStars.append(ratting);
+
+        const rattingNum = getElement('span', ['ratting'], {
+          textContent: item.number,
+        });
+        containerStars.append(rattingNum);
+      });
+    };
+ 
+    main.append(mainContainer);
+    mainContainer.append(containerAbout);
+    containerAbout.append(containerStars);
+    containerAbout.append(titleText);
+    containerAbout.append(pCnt);
+    containerAbout.append(buttonMain);
+    
+    return main;
+  };
+
   const movieConstructor = (selector, option) => {
     const app = document.querySelector(selector);
+
+    document.title = option.title;
 
     if (option.header) {
       app.append(createHeader(option));
@@ -101,6 +155,10 @@ new Swiper('.swiper', {
     if (option.backg) {
       document.body.style.background = option.backg;
     };
+
+    if (option.main) {
+      app.append(mainContent(option));
+    }
   };
 
   movieConstructor('.app', {
@@ -113,18 +171,27 @@ new Swiper('.swiper', {
         {
           title: 'twitter',
           link: '#',
-          img: '/img/twitter.svg'
+          img: '/img/twitter.svg',
         },
         {
           title: 'instagram',
           link: '#',
-          img: '/img/instagram.svg'
+          img: '/img/instagram.svg',
         },
         {
           title: 'facebook',
           link: '#',
-          img: '/img/facebook.svg'
+          img: '/img/facebook.svg',
         },
       ]
+    },
+    main: {
+      ganre: ['2019, фэнтези'],
+      ratting: [{
+        number: '7/10',
+        img: '/img/rating-stars.svg',
+      }],
+      filmAbout: ['Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.'],
+      youTubeTrailer: 'https://www.youtube.com/watch?v=MlpcwN_6rQs'
     }
   });
